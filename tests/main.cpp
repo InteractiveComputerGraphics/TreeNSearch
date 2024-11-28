0bf378bd37e972c267ee36f6247194c6f7c4ddb4
joseantfer <joseantfer.engineer@gmail.com>
2024-11-28 19:19:51 +0100

    tidier tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,18 +1,30 @@
 #include "tests.h"
 
 
+void tests(int n_points, bool crash_at_first_discrepancy)
+{
+	std::cout << "\n\nTests with " << n_points << " particles." << std::endl;
+	std::cout << "======================================" << std::endl;
+	one_set_fixed_radius(n_points, crash_at_first_discrepancy);
+	two_dynamic_sets_variable_radius(n_points, crash_at_first_discrepancy);
+	mixed_float_double_point_sets(n_points, crash_at_first_discrepancy);
+	resize_variable_radius(n_points, crash_at_first_discrepancy);
+}
+
 int main()
 {
-	const int n_points = 0;
-	bool report_when_a_test_fails = true;
+	// Note: Do not run TreeNSearch for 0 particles. 
+	// It will give a warning when run, but it will crash when queried 
+	// (e.g. unexisting particle neighbors or zsort an empty set).
+
+	bool crash_at_first_discrepancy = false;
 
 	// Tests
-	one_set_fixed_radius(n_points, report_when_a_test_fails);
-	two_dynamic_sets_variable_radius(n_points, report_when_a_test_fails);
-	mixed_float_double_point_sets(n_points, report_when_a_test_fails);
-	resize_variable_radius(n_points, report_when_a_test_fails);
+	tests(/* n_particles = */ 1, crash_at_first_discrepancy);
+	tests(/* n_particles = */ 100, crash_at_first_discrepancy);
+	tests(/* n_particles = */ 10000, crash_at_first_discrepancy);
 
-	// Benchmarks
+	// Benchmark
 	const int n_benchmark_points = 9000;
 	benchmark_one_dynamic_set(n_benchmark_points);
 }
